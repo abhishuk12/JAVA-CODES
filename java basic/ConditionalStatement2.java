c3b081badf8d211572206a2e7cf77c3e9a430a8d
sunny <sengersunny448@gmail.com>
2022-05-26 00:36:03 +0530

    conditional statement 2

---
+++ java basic/ConditionalStatement2.java
@@ -0,0 +1,17 @@
+public class ConditionalStatement2 {
+    private static void  main (String [] args) {
+        int a=18;
+        if(a<10){
+            System.out.println("the number is smaller than 10 ");
+
+        }
+        else if(age>=10 && age<=20){
+            System.out.println("the number is greater than 10 but less than 20 ");
+
+        }
+        else{
+            System.out.println("let's  code more ");
+        }
+    }
+    
+}
