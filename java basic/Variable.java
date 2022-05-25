29ad2dd9e8b436a72b243e30eca0334d55801dfd
sunny <sengersunny448@gmail.com>
2022-05-26 00:37:03 +0530

    variables in java

--- java basic/Variable.java
+++ java basic/Variable.java
@@ -1,4 +1,4 @@
-// this programs is abot how variables are declared in java 
+// this programs is about how variables are declared in java 
 public class Variable {
     public static void main(String [] args){
         int age=19;
