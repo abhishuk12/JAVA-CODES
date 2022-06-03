244a9bfbb3f293bad9378254d23f12f587b52f70
sunny <sengersunny448@gmail.com>
2022-06-03 12:13:55 +0530

    string intro

---
+++ STRINGS/String.java
@@ -0,0 +1,12 @@
+public class String {
+public static void main(String[] args){
+    String name="SUNNY"; //here the string is declare by using litrals 
+    System.out.println(name);
+    String name2 =new String("hey its sunny  here "); // here the string is declare by using  string class
+    System.out.println(name2);
+    System.out.println(name==name2) //it will return false because both the string are declare in diffrent memory location   
+
+
+      
+}
+}
